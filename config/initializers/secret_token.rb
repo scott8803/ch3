32d496c16535a1bcf73fe9ae02a14f1547f7ddfa
Scott Eisenberg <scott2@btrtrucks.com>
2011-12-31 12:31:28 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ch3::Application.config.secret_token = '3f5c75aa8790dbd908429edfdbd39078763834fb5536c72532c1c054d0bf8ea1ab96a2873dd4e06a31160376d1ca501af95caab447c16c4f77fefd6dd80c2c89'
