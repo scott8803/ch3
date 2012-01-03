51f4d3a945372e6bd37c64149ae16cd1be269367
Scott Eisenberg <scott2@btrtrucks.com>
2012-01-03 15:16:32 -0500

    Added signup controler and page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Ch3::Application.routes.draw do
+  get "users/new"
+
   get "pages/home"
   get "pages/contact"
   get "pages/about"
@@ -6,6 +8,7 @@ Ch3::Application.routes.draw do
 	match '/about',	:to => 'pages#about'
 	match '/contact',	:to => 'pages#contact'
 	match '/help',	:to => 'pages#help'
+	match '/signup',	:to => 'users#new'
 	root :to => 'pages#home'
 
   # The priority is based upon order of creation:
