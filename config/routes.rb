b817c63a20a61e8c720446818173509746e7d658
Scott Eisenberg <scott2@btrtrucks.com>
2012-01-07 22:09:01 -0500

    Merged branch

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Ch3::Application.routes.draw do
-  get "pages/home"
-  get "pages/contact"
-  get "pages/about"
-  get "pages/help"
-  get "users/new"
+	resources :users
   
 	match '/about',	:to => 'pages#about'
 	match '/contact',	:to => 'pages#contact'
