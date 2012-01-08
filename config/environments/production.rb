63bfd1f91b96fd0a30e287935d38e31cf6fed1c9
Scott Eisenberg <scott2@btrtrucks.com>
2012-01-07 22:53:13 -0500

    Try to avoid compile at runtime 2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -45,7 +45,7 @@ Ch3::Application.configure do
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
 
-  config.assets.compile = true
+  #config.assets.compile = true
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
