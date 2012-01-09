7ff3770f36a7603f3e3716fa9c9295e49186652e
Scott Eisenberg <scott2@btrtrucks.com>
2012-01-09 16:56:00 -0500

    Can create new user sign-up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,10 +2,21 @@ class UsersController < ApplicationController
 
 	def show 
 		@user = User.find(params[:id])
+		@title = @user.name
 	end
 	
   def new
   	@title = "Sign Up"
+  	@user = User.new
   end
-
+  
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      # Handle a successful save.
+    else
+      @title = "Sign up"
+      render 'new'
+    end
+  end
 end
